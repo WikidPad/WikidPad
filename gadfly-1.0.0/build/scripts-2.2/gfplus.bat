ce92046240b2129acf184f6029a9dd352c6f8a58
mbutscher <mbutscher@localhost>
2006-03-09 22:48:32 +0000

    Directory "branches/mbutscher/work/gadfly-1.0.0" deleted, couldn't see any use for it.

--- gadfly-1.0.0/build/scripts-2.2/gfplus.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-if NOT "%_4ver%" == "" c:\Python22\python.exe -c "from gadfly.scripts.gfplus import main; main()" %$
-if     "%_4ver%" == "" c:\Python22\python.exe -c "from gadfly.scripts.gfplus import main; main()" %*
