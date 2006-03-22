398ddba642605ce515f1f1fe5721a63a509f54aa
mbutscher <mbutscher@localhost>
2006-03-22 22:54:06 +0000

    Directory "gadfly-1.0.0" deleted from "stable-1.6" branch

--- gadfly-1.0.0/build/scripts-2.3/gfserver.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-if NOT "%_4ver%" == "" C:\Python23\python.exe -c "from gadfly.scripts.gfserver import main; main()" %$
-if     "%_4ver%" == "" C:\Python23\python.exe -c "from gadfly.scripts.gfserver import main; main()" %*
--- gadfly-1.0.0/kjbuckets/setup.py
+++
@@ -1,7 +0,0 @@
-#! /usr/local/bin/python -O
-
-from distutils.core import setup, Extension
-
-setup (name = "kjbuckets",
-   version = "2.2",
-   ext_modules = [Extension("kjbuckets", ["kjbucketsmodule.c"])])
