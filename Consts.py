5591eddeeec80fb06d3b92656be3db82d141e30f
mbutscher <mbutscher@localhost>
2009-12-06 15:28:39 +0000

    branches/stable-1.9: 1.9rc11
    * Bug fixed: Linux: Detection of Python version for
      Sqlite may fail (thanks to Martin Muenstermann)
    
    
    branches/mbutscher/work:
    2.0beta10
    
    * Bug fixed: Linux: Detection of Python version for
      Sqlite may fail (thanks to Martin Muenstermann)
    * Bug fixed: Opening of Sqlite databases with
      foreign characters in path may fail 
    * Bug fixed: Resizing of internal HTML preview
      sometimes doesn't work, produces gray borders

--- Consts.py
+++ Consts.py
@@ -19,6 +19,6 @@
 # (1, 9, 4, 2) is something after 1.9beta4
 # (2, 0, 200, 0) is 2.0final
 
-VERSION_TUPLE = ("wikidPad", 1, 9, 110, 0)
-VERSION_STRING = "wikidPad 1.9rc10"
+VERSION_TUPLE = ("wikidPad", 1, 9, 111, 0)
+VERSION_STRING = "wikidPad 1.9rc11"
 HOMEPAGE = u"http://wikidpad.sourceforge.net"
