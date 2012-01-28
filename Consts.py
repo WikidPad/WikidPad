83ccd5d541fe951d052f5a785d801143e504c2d8
mbutscher <mbutscher@localhost>
2012-01-28 21:53:45 +0000

    branches/stable-2.2: 2.2rc02
    branches/mbutscher/work:
    2.3beta02
    * Fixed bug in HTML output for multi-cell spans

--- Consts.py
+++ Consts.py
@@ -23,8 +23,8 @@ from pwiki.Enum import Enumeration
 # (1, 9, 104, 2) is something after 1.9beta04
 # (2, 0, 300, 0) is 2.0final
 
-VERSION_TUPLE = ("wikidPad", 2, 2, 201, 0)
-VERSION_STRING = "wikidPad 2.2rc01"
+VERSION_TUPLE = ("wikidPad", 2, 2, 202, 0)
+VERSION_STRING = "wikidPad 2.2rc02"
 HOMEPAGE = u"http://wikidpad.sourceforge.net"
 
 CONFIG_FILENAME = "WikidPad.config"
