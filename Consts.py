fc7a4f2cdb889676d0d236d7eaf3a4fc2af0ec4b
mbutscher <mbutscher@localhost>
2014-02-15 11:13:24 +0000

    branches/mbutscher/work: 2.3beta12
    branches/mbutscher/stable-2.2:
    2.2 final

--- Consts.py
+++ Consts.py
@@ -23,8 +23,8 @@ from pwiki.Enum import Enumeration
 # (1, 9, 104, 2) is something after 1.9beta04
 # (2, 0, 300, 0) is 2.0final
 
-VERSION_TUPLE = ("wikidPad", 2, 3, 111, 0)
-VERSION_STRING = "wikidPad 2.3beta11"
+VERSION_TUPLE = ("wikidPad", 2, 3, 112, 0)
+VERSION_STRING = "wikidPad 2.3beta12"
 HOMEPAGE = u"http://wikidpad.sourceforge.net"
 
 CONFIG_FILENAME = "WikidPad.config"
