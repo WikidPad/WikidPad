663c4d5d2341ef71bdd0b54468a38286358f065c
Michael Butscher <mbutscher@gmx.de>
2018-01-08 00:27:07 +0100

    Support for building Windows binary with py2exe with Python 3.4
    This needs a modified py2exe. Used version at the time of this commit is at:
    
    https://github.com/WikidPad/Accessory-py2exe/tree/16924397248156269e19c6d620390bfc8cab18e7

--- bld.bat
+++ bld.bat
@@ -1,7 +1,5 @@
 SET PYTHONPATH=lib
 del icons\Thumbs.db
-python buildGadflyZips.py
 python updateI18N.py
 python setup.py py2exe
 copy license.txt dist\license.txt
-copy gadfly_small.zip dist\gadfly.zip
