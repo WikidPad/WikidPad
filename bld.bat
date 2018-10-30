68f413a40fd32538ee52cee18b5eacc73a4931b2
Michael Butscher <mbutscher@gmx.de>
2018-10-30 23:44:37 +0100

    Copying of Windows binary specific data placed in bld.bat

--- bld.bat
+++ bld.bat
@@ -1,5 +1,23 @@
-SET PYTHONPATH=lib
-del icons\Thumbs.db
+SET PYTHONPATH=WikidPad\lib;WikidPad
+del WikidPad\icons\Thumbs.db
+
+cd WikidPad
 python updateI18N.py
+cd ..
+
 python setup.py py2exe
-copy license.txt dist\license.txt
+
+
+copy WikidPad\license.txt dist\license.txt
+xcopy WikidPad\icons\* dist\icons /i
+xcopy WikidPad\extensions dist\extensions /i /s
+xcopy sqlite3.dll dist /i /y
+xcopy WikidPad\WikidPad.xrc dist /i /y
+xcopy WikidPad\langlist.txt dist /i /y
+xcopy WikidPad\appbase.css dist /i /y
+xcopy WikidPad\WikidPad_*.po dist /i /y
+xcopy WikidPad\WikidPadHelp dist\WikidPadHelp /i /s
+
+REM python WikidPad\updateI18N.py
+REM python setup.py py2exe
+REM copy WikidPad\license.txt ..\dist\license.txt
