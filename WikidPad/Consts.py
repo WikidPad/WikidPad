aa747bb42cf55c965a1ff7cac7a011a66ce9774d
Michael Butscher <mbutscher@gmx.de>
2018-10-30 23:43:28 +0100

    Including Enumeration class in Consts.py (not needed elsewhere). This should allow to import Consts.py without setting import paths.

--- WikidPad/Consts.py
+++ WikidPad/Consts.py
@@ -1,4 +1,51 @@
-from pwiki.Enum import Enumeration
+# Code for enumeration is directly placed here instead of an import
+# because same import doesn't work if the Const module is imported from application
+# and from setup.py
+
+# Moreover, Enumeration isn't used in any other module
+
+import types, pprint
+
+class EnumException(Exception):
+    pass
+
+class Enumeration:
+    def __init__(self, name, enumList, startAt=0):
+        self.__doc__ = name
+        lookup = { }
+        reverseLookup = { }
+        i = startAt
+        uniqueNames = [ ]
+        uniqueValues = [ ]
+        for x in enumList:
+            if type(x) == tuple:
+                x, i = x
+            if type(x) != str:
+                raise EnumException("enum name is not a string: " + x)
+            if type(i) != int:
+                raise EnumException("enum value is not an integer: " + i)
+            if x in uniqueNames:
+                raise EnumException("enum name is not unique: " + x)
+            if i in uniqueValues:
+                raise EnumException("enum value is not unique for " + x)
+            uniqueNames.append(x)
+            uniqueValues.append(i)
+            lookup[x] = i
+            reverseLookup[i] = x
+            i = i + 1
+        self.lookup = lookup
+        self.reverseLookup = reverseLookup
+    def __getattr__(self, attr):
+        if attr not in self.lookup:
+            raise AttributeError
+            
+        setattr(self, attr, self.lookup[attr])
+        return self.lookup[attr]
+    def whatis(self, value):
+        return self.reverseLookup[value]
+
+
+# ----- End of Enumeration inclusion -----
 
 
 # VERSION_TUPLE is structured (branch, major, minor, stateAndMicro, patch)
