d99aac70f0b53ef18465f02872d0e1396c50e8bd
Michael Butscher <mbutscher@gmx.de>
2019-01-15 04:16:44 +0100

    Instead of explicit encoding use file in binary mode and let Python's load mechanism handle the encoding.

--- WikidPad/lib/pwiki/PluginManager.py
+++ WikidPad/lib/pwiki/PluginManager.py
@@ -337,7 +337,7 @@ class PluginManager:
                         continue
                     if os.path.isfile(fullname):
                         if ext == '.py':
-                            with open(fullname,encoding="utf-8") as f:   # HACK changed here !
+                            with open(fullname, "rb") as f:
                                 module = imp.load_module(packageName + "." + moduleName, f,
                                         fullname, (".py", "r", imp.PY_SOURCE))
                         elif ext == '.zip':
